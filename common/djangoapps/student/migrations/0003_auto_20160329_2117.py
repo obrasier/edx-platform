1b2506fae6e0daff4894f7c465b7d71d68263e25
Owen Brasier <obrasier@gmail.com>
2016-03-30 01:25:58 +0000

    ran migrations removing the localflavour stuff, added the student migration folder to .gitignore

--- common/djangoapps/student/migrations/0003_auto_20160329_2117.py
+++ common/djangoapps/student/migrations/0003_auto_20160329_2117.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 from django.conf import settings
-import localflavor.au.models
 import xmodule_django.models
 
 
@@ -20,11 +19,17 @@ class Migration(migrations.Migration):
             name='ClassSet',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('short_name', models.CharField(max_length=12)),
-                ('class_name', models.CharField(max_length=12)),
+                ('class_code', models.CharField(unique=True, max_length=9, db_index=True)),
+                ('short_name', models.CharField(max_length=12, null=True)),
+                ('class_name', models.CharField(max_length=50, null=True)),
                 ('course_id', xmodule_django.models.CourseKeyField(db_index=True, max_length=255, blank=True)),
+                ('grade', models.CharField(max_length=12, null=True)),
                 ('assessment', models.BooleanField(default=False)),
+                ('no_of_students', models.IntegerField(null=True)),
             ],
+            options={
+                'abstract': False,
+            },
         ),
         migrations.CreateModel(
             name='ClassTime',
@@ -46,26 +51,20 @@ class Migration(migrations.Migration):
                 ('school_name', models.CharField(max_length=100)),
                 ('street_address', models.CharField(max_length=100)),
                 ('suburb', models.CharField(max_length=100)),
-                ('state', localflavor.au.models.AUStateField(max_length=3, choices=[(b'ACT', b'Australian Capital Territory'), (b'NSW', b'New South Wales'), (b'NT', b'Northern Territory'), (b'QLD', b'Queensland'), (b'SA', b'South Australia'), (b'TAS', b'Tasmania'), (b'VIC', b'Victoria'), (b'WA', b'Western Australia')])),
-                ('postcode', localflavor.au.models.AUPostCodeField(max_length=4)),
+                ('state', models.CharField(max_length=10)),
+                ('postcode', models.CharField(max_length=10)),
                 ('school_sector', models.CharField(max_length=1, choices=[(b'G', b'Government'), (b'C', b'Catholic'), (b'I', b'Independent')])),
                 ('school_type', models.CharField(max_length=1, choices=[(b'P', b'Primary'), (b'S', b'Secondary'), (b'C', b'Combined'), (b'O', b'Special')])),
-            ],
-        ),
-        migrations.CreateModel(
-            name='SchoolGrade',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('description', models.CharField(max_length=12)),
-                ('default_list', models.BooleanField(default=False)),
             ],
         ),
         migrations.CreateModel(
             name='StudentProfile',
             fields=[
                 ('user', models.OneToOneField(primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
-                ('year_of_birth', models.IntegerField(db_index=True, null=True, blank=True)),
+                ('school_grade', models.CharField(blank=True, max_length=2, choices=[(b'K', b'K'), (b'1', b'Year 1'), (b'2', b'Year 2'), (b'3', b'Year 3'), (b'4', b'Year 4'), (b'5', b'Year 5'), (b'6', b'Year 6'), (b'7', b'Year 7'), (b'8', b'Year 8'), (b'9', b'Year 9'), (b'10', b'Year 10'), (b'11', b'Year 11'), (b'12', b'Year 12'), (b'T', b'Tertiary')])),
                 ('indigenous', models.BooleanField(default=False)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('classSet', models.ManyToManyField(to='student.ClassSet')),
             ],
         ),
@@ -73,7 +72,7 @@ class Migration(migrations.Migration):
             name='Subject',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('description', models.CharField(max_length=12)),
+                ('description', models.CharField(max_length=30)),
                 ('default_list', models.BooleanField(default=False)),
             ],
         ),
@@ -81,8 +80,10 @@ class Migration(migrations.Migration):
             name='TeacherProfile',
             fields=[
                 ('user', models.OneToOneField(primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
-                ('phone', localflavor.au.models.AUPhoneNumberField(max_length=20, null=True)),
+                ('phone', models.CharField(max_length=25, null=True)),
                 ('hear_about_us', models.CharField(blank=True, max_length=2, null=True, choices=[(b'MM', b'Participated in MadMaker 2015'), (b'FR', b'Friend'), (b'FA', b'Family Member'), (b'CO', b'Colleague'), (b'SY', b'Sydney Uni Marketing & Comms'), (b'SM', b'Social Media'), (b'SE', b'Search Engine'), (b'CE', b'Conference or Event'), (b'O', b'Other')])),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('school', models.ForeignKey(to='student.School', null=True)),
             ],
         ),
@@ -93,18 +94,13 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='classset',
-            name='grade',
-            field=models.ManyToManyField(to='student.SchoolGrade'),
-        ),
-        migrations.AddField(
-            model_name='classset',
             name='school',
             field=models.ForeignKey(to='student.School'),
         ),
         migrations.AddField(
             model_name='classset',
             name='subject',
-            field=models.ManyToManyField(to='student.Subject'),
+            field=models.ManyToManyField(to='student.Subject', blank=True),
         ),
         migrations.AddField(
             model_name='classset',
